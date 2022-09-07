f2ec8c08d32ee5bbbe6ee28fb30ce4f5a80f70a9
Vokler <valerypavlikov@yandex.ru>
2022-09-07 19:53:02 +0300

    removed logging

--- bot/views.py
+++ bot/views.py
@@ -18,14 +18,6 @@ from bot.apps import BotConfig
 
 GENDER, PHOTO, LOCATION, BIO = range(4)
 
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-
-logger = logging.getLogger(__name__)
-
-
 def start(update: Update, context: CallbackContext) -> int:
     """Starts the conversation and asks the user about their gender."""
     reply_keyboard = [['Boy', 'Girl', 'Other']]
@@ -44,7 +36,7 @@ def start(update: Update, context: CallbackContext) -> int:
 def gender(update: Update, context: CallbackContext) -> int:
     """Stores the selected gender and asks for a photo."""
     user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    print("Gender of %s: %s", user.first_name, update.message.text)
     update.message.reply_text(
         'I see! Please send me a photo of yourself, '
         'so I know what you look like, or send /skip if you don\'t want to.',
@@ -59,7 +51,7 @@ def photo(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     photo_file = update.message.photo[-1].get_file()
     photo_file.download('user_photo.jpg')
-    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    print("Photo of %s: %s", user.first_name, 'user_photo.jpg')
     update.message.reply_text(
         'Gorgeous! Now, send me your location please, or send /skip if you don\'t want to.'
     )
@@ -70,7 +62,7 @@ def photo(update: Update, context: CallbackContext) -> int:
 def skip_photo(update: Update, context: CallbackContext) -> int:
     """Skips the photo and asks for a location."""
     user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
+    print("User %s did not send a photo.", user.first_name)
     update.message.reply_text(
         'I bet you look great! Now, send me your location please, or send /skip.'
     )
@@ -82,7 +74,7 @@ def location(update: Update, context: CallbackContext) -> int:
     """Stores the location and asks for some info about the user."""
     user = update.message.from_user
     user_location = update.message.location
-    logger.info(
+    print(
         "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
     )
     update.message.reply_text(
@@ -95,7 +87,7 @@ def location(update: Update, context: CallbackContext) -> int:
 def skip_location(update: Update, context: CallbackContext) -> int:
     """Skips the location and asks for info about the user."""
     user = update.message.from_user
-    logger.info("User %s did not send a location.", user.first_name)
+    print("User %s did not send a location.", user.first_name)
     update.message.reply_text(
         'You seem a bit paranoid! At last, tell me something about yourself.'
     )
@@ -106,7 +98,7 @@ def skip_location(update: Update, context: CallbackContext) -> int:
 def bio(update: Update, context: CallbackContext) -> int:
     """Stores the info about the user and ends the conversation."""
     user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
+    print("Bio of %s: %s", user.first_name, update.message.text)
     update.message.reply_text('Thank you! I hope we can talk again some day.')
 
     return ConversationHandler.END
@@ -115,7 +107,7 @@ def bio(update: Update, context: CallbackContext) -> int:
 def cancel(update: Update, context: CallbackContext) -> int:
     """Cancels and ends the conversation."""
     user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
+    print("User %s canceled the conversation.", user.first_name)
     update.message.reply_text(
         'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
     )
