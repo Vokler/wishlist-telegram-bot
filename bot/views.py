b6c314fb45c018e57807cfa668e9b3bc84170213
Vokler <valerypavlikov@yandex.ru>
2022-09-07 15:04:16 +0300

    added ConvHandler

--- bot/views.py
+++ bot/views.py
@@ -7,12 +7,111 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 
-from telegram.ext import CommandHandler, Dispatcher
-from telegram import Bot, Update
+from telegram.ext import (CommandHandler, Dispatcher,
+                          MessageHandler,
+                          Filters,
+                          ConversationHandler,
+                          CallbackContext, )
+from telegram import Bot, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+
+GENDER, PHOTO, LOCATION, BIO = range(4)
 
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+def start(update: Update, context: CallbackContext) -> int:
+    """Starts the conversation and asks the user about their gender."""
+    reply_keyboard = [['Boy', 'Girl', 'Other']]
+
+    update.message.reply_text(
+        'Hi! My name is Professor Bot. I will hold a conversation with you. '
+        'Send /cancel to stop talking to me.\n\n'
+        'Are you a boy or a girl?',
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder='Boy or Girl?'
+        ),
+    )
+    return GENDER
+
+
+def gender(update: Update, context: CallbackContext) -> int:
+    """Stores the selected gender and asks for a photo."""
+    user = update.message.from_user
+    print("Gender of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text(
+        'I see! Please send me a photo of yourself, '
+        'so I know what you look like, or send /skip if you don\'t want to.',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return PHOTO
+
+
+def photo(update: Update, context: CallbackContext) -> int:
+    """Stores the photo and asks for a location."""
+    user = update.message.from_user
+    photo_file = update.message.photo[-1].get_file()
+    photo_file.download('user_photo.jpg')
+    print("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    update.message.reply_text(
+        'Gorgeous! Now, send me your location please, or send /skip if you don\'t want to.'
+    )
+
+    return LOCATION
+
+
+def skip_photo(update: Update, context: CallbackContext) -> int:
+    """Skips the photo and asks for a location."""
+    user = update.message.from_user
+    print("User %s did not send a photo.", user.first_name)
+    update.message.reply_text(
+        'I bet you look great! Now, send me your location please, or send /skip.'
+    )
+
+    return LOCATION
+
+
+def location(update: Update, context: CallbackContext) -> int:
+    """Stores the location and asks for some info about the user."""
+    user = update.message.from_user
+    user_location = update.message.location
+    print(
+        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
+    )
+    update.message.reply_text(
+        'Maybe I can visit you sometime! At last, tell me something about yourself.'
+    )
+
+    return BIO
+
+
+def skip_location(update: Update, context: CallbackContext) -> int:
+    """Skips the location and asks for info about the user."""
+    user = update.message.from_user
+    print("User %s did not send a location.", user.first_name)
+    update.message.reply_text(
+        'You seem a bit paranoid! At last, tell me something about yourself.'
+    )
+
+    return BIO
+
+
+def bio(update: Update, context: CallbackContext) -> int:
+    """Stores the info about the user and ends the conversation."""
+    user = update.message.from_user
+    print("Bio of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('Thank you! I hope we can talk again some day.')
+
+    return ConversationHandler.END
+
+
+def cancel(update: Update, context: CallbackContext) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    print("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text(
+        'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
 
 
 @csrf_exempt
@@ -22,8 +121,21 @@ def process(request):
     dispatcher = Dispatcher(bot, update_queue)
 
     # Register handlers here
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            GENDER: [MessageHandler(Filters.regex('^(Boy|Girl|Other)$'), gender)],
+            PHOTO: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
+            LOCATION: [
+                MessageHandler(Filters.location, location),
+                CommandHandler('skip', skip_location),
+            ],
+            BIO: [MessageHandler(Filters.text & ~Filters.command, bio)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    dispatcher.add_handler(conv_handler)
 
     # Start the thread
     thread = Thread(target=dispatcher.start, name='dispatcher')
