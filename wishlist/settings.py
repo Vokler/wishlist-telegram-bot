cf5d71fc6e860f1564464deb9ea23655a63960a7
Vokler <valerypavlikov@yandex.ru>
2022-10-18 22:43:09 +0300

    STATIC_URL back

--- wishlist/settings.py
+++ wishlist/settings.py
@@ -139,6 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = BASE_DIR / 'static'
 
