9f40ae4a6c1d5b401bcd6870535ba258e1661978
Vokler <valerypavlikov@yandex.ru>
2022-10-18 21:15:28 +0300

    prepare for deploy

--- wishlist/settings.py
+++ wishlist/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'wishlist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wish_list',
+        'USER': 'wish_list_user',
+        'PASSWORD': 'wish_list_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
