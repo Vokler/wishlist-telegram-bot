3f33535d816250829b0b2cbdba4ed3e4d7d8b233
Vokler <valerypavlikov@yandex.ru>
2022-11-16 15:34:55 +0200

    added logger for process() view

--- bot/views.py
+++ bot/views.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -6,10 +7,13 @@ from telegram import Update
 
 from bot.tg_init import BOT, DISPATCHER
 
+logger = logging.getLogger(__name__)
+
 
 @csrf_exempt
 def process(request):
-    data = json.loads(request.body)
+    logger.warning(f'REQUEST: {request.body.decode()}')
+    data = json.loads(request.body.decode())
     update = Update.de_json(data, BOT)
     DISPATCHER.process_update(update)
 
